8055001c8795130e5b9d55ee5f53f0389c554e8f
Jesse Gallagher <jesse@frostillic.us>
2014-05-28 11:18:59 -0400

    Converted Forms over to the OpenNTF Domino API and added interop with Enum fields

---
+++ forms-n-views/Code/Java/converter/EnumBindingConverter.java
@@ -0,0 +1,22 @@
+package converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.el.ValueBinding;
+
+public class EnumBindingConverter implements Converter {
+
+	@SuppressWarnings("unchecked")
+	public Object getAsObject(final FacesContext facesContext, final UIComponent component, final String value) {
+		ValueBinding binding = component.getValueBinding("value");
+		Class<Enum> enumType = binding.getType(facesContext);
+
+		return Enum.valueOf(enumType, value);
+	}
+
+	public String getAsString(final FacesContext facesContext, final UIComponent component, final Object value) {
+		return String.valueOf(value);
+	}
+
+}
--- forms-n-views/Code/Java/frostillicus/DesignAssistant.java
+++ forms-n-views/Code/Java/frostillicus/DesignAssistant.java
@@ -1,11 +1,47 @@
 package frostillicus;
+
 import java.io.Serializable;
+import java.lang.reflect.Method;
 
 import org.openntf.domino.*;
 import org.openntf.domino.design.*;
 
 public class DesignAssistant implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	public static String createElement(final String databaseDocumentId, final String name, final String type) {
+		try {
+			Database foreignDB = FNVUtil.getDatabaseForDocumentId(databaseDocumentId);
+			DatabaseDesign design = foreignDB.getDesign();
+
+			Method createElementMethod = design.getClass().getMethod("create" + type);
+			Object newElement = createElementMethod.invoke(design);
+			Method setNameMethod = newElement.getClass().getMethod("setName", String.class);
+			setNameMethod.invoke(newElement, name);
+			Method saveMethod = newElement.getClass().getMethod("save");
+			saveMethod.invoke(newElement);
+			Method getUniversalIDMethod = newElement.getClass().getMethod("getUniversalID");
+			return (String)getUniversalIDMethod.invoke(newElement);
+
+		} catch(Exception e) {
+			e.printStackTrace();
+			FNVUtil.alert(e.getMessage());
+		}
+		return null;
+	}
+	public static DesignBase getElement(final String databaseDocumentId, final String name, final String type) {
+		try {
+			Database foreignDB = FNVUtil.getDatabaseForDocumentId(databaseDocumentId);
+			DatabaseDesign design = foreignDB.getDesign();
+
+			Method getElementMethod = design.getClass().getMethod("get" + type, String.class);
+			return (DesignBase)getElementMethod.invoke(design, name);
+		} catch(Exception e) {
+			e.printStackTrace();
+			FNVUtil.alert(e.getMessage());
+		}
+		return null;
+	}
 
 	public static String createView(final String databaseDocumentId, final String name) {
 		try {
